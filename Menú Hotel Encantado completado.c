68fed4c2410f88544efb9e8065add878d335b983
ana4centeno <an.centeno@alumnos.upm.es>
2022-05-24 20:46:45 +0200

    primera parte nivel dificil

--- Menú Hotel Encantado completado.c
+++ Menú Hotel Encantado completado.c
@@ -55,6 +55,51 @@ int main ()
                 break;
             case 3:
                 printf("Ha elegido la dificultad dificil");
+    char x;
+    float solucion1=1000,solucion2=17,solucion3=3748;
+    int cont=0;
+    printf("Estas en el momento mas complicado de esta historia,espero que estes preparado...\n");
+    scanf("%c",&x);
+    printf("Se trata de la ultima planta de este horrible hotel del que estas deseando escapar.\n");
+    printf("Antes de entrar a las habitaciones y encontrar la salida,debes resolver esta prueba para comprobar si estas al nivel\n");
+    scanf("%c",&x);
+    printf("Aqui tienes una baraja de 52 cartas, dividas en dos mazos de 26 cartas cada uno.Si comprobaras las cartas que contiene  cada mazo 1000 veces");
+    printf("¿cuántas veces crees que coincidiría el número de cartas rojas de un mazo con el número de cartas   negras del otro mazo?\n");
+    cont=preguntamatematicas(solucion1);
+    printf("Vidas perdidas %i\n",cont);
+    if(cont==3){
+        printf("Lamento decirte que no has logrado superar ni si quiera esta sencilla prueba,por lo que a continuacion seras arrojado al vacio junto con los demas jugadores que lo han intentado");
+        printf("FIN DEL JUEGO");
+    }
+    else{
+        printf("mmm...observo que eres un jugador astuto espero que estes preparado para entrar a la primera habitacion\n");
+        scanf("%c",&x);
+    }
+    printf("(Se abre la primera puerta...)");
+    scanf("%c",&x);
+    printf("Hay un chico que está sentado en una esquina tirando un dado. Cada vez que recoge el dado, lo mira y murmura algo.\n");
+    printf("Decides acercarte para escuchar lo que dice y parece repetir lo siguinte una y otra vez...:\n");
+    printf("Cada vez que sale un 1, dice: 15.Cada vez que sale un 6, dice: 20.Repite esto una y otra vez.\n");
+    printf("Hasta que de repente sale un 3 y el chico te observa y espera tu respuesta que responderías");
+    cont=preguntamatematicas(solucion2);
+    printf("Vidas perdidas %i\n",cont);
+    if(cont==3){
+        printf("Lamento decirte que no has logrado superar la prueba del chico,por lo que este decide devorarte");
+        printf("FIN DEL JUEGO");
+    }
+    else{
+        printf("El chico misteriso cambia su cara.Al parecer llevaba 50 años atrapado intentado resolver el acertijo.Este decide enseñarte cual es el siguiente paso para huir. \n");
+        scanf("%c",&x);}
+    printf("Segun el chico has comenzado con buen pie pero dice que no te confies que esta planta es realmente peligrosa...Te entrega un papel.\n");
+    scanf("%c",&x);
+    printf("Estas delante de la puerta a la que el chico te habia mandado , pero no puedes entrar esta bloqueada necesita que introduzcas un codigo\n");
+    printf("observando la puerta ves una serie de marcas '(X-X/X)*X=10' CLAVE:XXXX, el chico te dio un papel con una serie de numeros: 3,4,7,8.(Debes introducir los numeros ordenados para que cumplan la ecucacion ");
+    cont=preguntamatematicas(solucion3);
+    printf("Vidas perdidas %i\n",cont);
+    if(cont==3){
+        printf("Lamento decirte que no has logrado abrir esta puerta,por lo que seras arrojado al vacio junto con los demas jugadores que lo han intentado ");
+        printf("FIN DEL JUEGO");}
+
                 break;
             case 4:
                 printf("Ha elegido salir.\n¡Vuelva pronto!\n\n");
